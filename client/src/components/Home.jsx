736ea7544160022851bf12c0588d96184496647c
Kishor <76237264+ExoticFormula@users.noreply.github.com>
2022-09-05 23:42:59 +0530

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,14 +5,6 @@ import TopAiringSection from "./TopAiringSection";
 import UpcomingSection from "./UpcomingSection";
 import PopularSection from "./PopularSection";
 import FavoriteSection from "./FavoriteSection";
-// import TrendingSection from "./TrendingSection";
-// import ModalVideo from "react-modal-video";
-// const playTrailer = () => {
-//   setIsPlaying(true);
-// };
-// const [trailerId, setTrailerId] = useState(trailerVideoId);
-// const [isPlaying, setIsPlaying] = useState(false);
-
 
 export default function Home() {
   return (
@@ -22,17 +14,7 @@ export default function Home() {
       <FavoriteSection></FavoriteSection>
       <TopAiringSection></TopAiringSection>
       <PopularSection></PopularSection>
-{/* 
-      <ModalVideo
 
-      
-        style={{ background: "red" }}
-        channel="youtube"
-        autoplay
-        isOpen={isPlaying}
-        videoId={trailerId}
-        onClose={() => setIsPlaying(false)}
-      /> */}
     </>
   );
 }
